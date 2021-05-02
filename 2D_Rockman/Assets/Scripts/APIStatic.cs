596e164a28fbb253c4a93caaba61b6365b530670
Futsunoja <57282808+Futsunoja@users.noreply.github.com>
2021-05-02 17:03:26 +0800

    靜態API練習

--- 2D_Rockman/Assets/Scripts/APIStatic.cs
+++ 2D_Rockman/Assets/Scripts/APIStatic.cs
@@ -10,7 +10,7 @@ public class APIStatic : MonoBehaviour
         //練習取得靜態屬性 Static Properties
         //語法
         //類別名稱.靜態屬性明稱
-        float r = Random.value;
+        float r = Random.value;    //隨機.值
         print("隨機值" + r);
 
         //練習存放靜態屬性 Static Properties
@@ -18,5 +18,23 @@ public class APIStatic : MonoBehaviour
         //語法
         //類別名稱.靜態屬性名稱 指定 值
         Cursor.visible = false;    //指標.可見度
+
+        //練習使用靜態的方法 Static Methods
+        //語法
+        //類別名稱.靜態方法(對應的參數)
+        int attack = Random.Range(100, 300);
+        print("隨機攻擊力：" + attack);
+
+        float n = Mathf.Abs(-99.9f);
+        print("絕對值：" + n);
+
+        print("攝影機數量：" + Camera.allCamerasCount);
+        print("重力大小：" + Physics2D.gravity);
+    }
+
+    private void Update()
+    {
+        print("是否按下任意按鍵：" + Input.anyKeyDown);
+        print("遊戲時間：" + Time.time);
     }
 }
